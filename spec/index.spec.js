409b3925bcf56e0b4f5cf489ec67de874c90d3d7
abbr <wenfred@gmail.com>
2014-04-07 14:47:20 -0700

    added usage testing.

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,40 +1,65 @@
 'use strict';
 
 function hookStdout(callback) {
-	var oldWrite = process.stdout.write;
-	// jshint unused: false
-	process.stdout.write = (function(write) {
-		return function(string, encoding, fd) {
-			//write.apply(process.stdout, arguments);
-			callback(string, encoding, fd);
-		};
-	})(process.stdout.write);
+  var oldWrite = process.stdout.write;
+  // jshint unused: false
+  process.stdout.write = (function(write) {
+    return function(string, encoding, fd) {
+      // write.apply(process.stdout, arguments);
+      callback(string, encoding, fd);
+    };
+  })(process.stdout.write);
 
-	return function() {
-		process.stdout.write = oldWrite;
-	};
+  return function() {
+    process.stdout.write = oldWrite;
+  };
 }
 
-describe('Index.js', function() {
-	var F;
-	process.argv.push('--help');
-	F = require('../lib/index');
-	var o = new F({
-		a : 'pass',
-		usage : 'sdf'
-	});
-	var outStr, unhook;
-	beforeEach(function() {
-		outStr = '';
-		unhook = hookStdout(function(string) {
-			outStr += string;
-		});
-	});
-	afterEach(function() {
-		unhook();
-	});
-	it('test1', function() {
-		o.getOpts();
-		expect(outStr).toContain('Print detailed help screen');
-	});
+describe('Invoking lib/index.js,', function() {
+  var F;
+  F = require('../lib/index');
+  var o = new F({
+    a : 'pass',
+    usage : 'Usage: ...'
+  });
+  var outStr, unhook;
+  beforeEach(function() {
+    outStr = '';
+    unhook = hookStdout(function(string) {
+      outStr += string;
+    });
+  });
+  afterEach(function() {
+    unhook();
+  });
+
+  describe('passing argument --usage,', function() {
+    var oldArgv;
+    beforeEach(function() {
+      oldArgv = process.argv;
+      process.argv = [ 'node', __filename, '--usage' ];
+    });
+    afterEach(function() {
+      process.argv = oldArgv;
+    });
+    it('calling method getOpts()', function() {
+      o.getOpts();
+      expect(outStr).toContain('Usage:');
+    });
+  });
+
+  describe('passing argument --help,', function() {
+    var oldArgv;
+    beforeEach(function() {
+      oldArgv = process.argv;
+      process.argv = [ 'node', __filename, '--help' ];
+    });
+    afterEach(function() {
+      process.argv = oldArgv;
+    });
+    it('calling method getOpts()', function() {
+      o.getOpts();
+      expect(outStr).toContain('Print detailed help screen');
+    });
+  });
 });
