32a41f3cd60c2502efac4239d0496738b99ec21a
abbr <wenfred@gmail.com>
2014-04-12 13:10:16 -0700

    added complete example

--- test.js
+++ test.js
@@ -5,7 +5,47 @@ var o = new F({
 });
 o.addArg({
 	'spec' : 'wget=<STRING>',
-	'help' : 'arguments passed to wget',
+	'help' : 'Arguments passed to wget',
 	'required' : true
 });
+o.addArg({
+	'spec' : 'm|match=<STRING>',
+	'help' : 'String response body must match'
+});
+o.addArg({
+	'spec' : 'w|warning=<STRING>',
+	'help' : 'Warning threshold'
+});
+o.addArg({
+	'spec' : 'c|critical=<STRING>',
+	'help' : 'Critical threshold'
+});
 o.getOpts();
+o.setThresholds({
+	'critical' : o.get('critical') || 2,
+	'warning' : o.get('warning') || 0.2
+});
+var exec = require('child_process').exec;
+var before = new Date().getTime();
+exec('wget -qO- ' + o.get('wget'), function(error, stdout, stderr) {
+	var after = new Date().getTime();
+	var diff = (after - before) / 1000;
+	var state = o.checkThreshold(diff);
+	o.addMessage(state, stdout.length + ' bytes in ' + diff
+			+ ' seconds response time.');
+	o.addPerfData({
+		label : "time",
+		value : diff,
+		uom : "s",
+		threshold : o.threshold,
+		min : 0
+	});
+	o.addPerfData({
+		label : "size",
+		value : stdout.length,
+		uom : "B",
+		min : 0
+	});
+	var messageObj = o.checkMessages();
+	o.nagiosExit(messageObj.state, messageObj.message);
+});
