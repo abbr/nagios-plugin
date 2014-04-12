5f21a5474c98e8e412c50330b7a03214ba08dc80
abbr <wenfred@gmail.com>
2014-04-11 17:24:33 -0700

    check for required arguments.

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -26,19 +26,20 @@ function hookStdout(callback) {
 	};
 }
 
-describe('lib/index.js,', function() {
+describe('lib/index.js', function() {
 	var F;
 	F = require('../lib/index');
-	var o = new F({
-		a : 'pass',
-		usage : 'Usage: ...'
-	});
-	var outStr, unhook;
+	var outStr, unhook, o;
 	beforeEach(function() {
+		o = new F({
+			a : 'pass',
+			usage : 'Usage: ...'
+		});
 		outStr = '';
 		unhook = hookStdout(function(string) {
 			outStr += string;
 		});
+		spyOn(process, 'exit');
 	});
 	afterEach(function() {
 		unhook();
@@ -65,7 +66,6 @@ describe('lib/index.js,', function() {
 			process.argv = oldArgv;
 		});
 		it('calls method getOpts()', function() {
-			spyOn(process, 'exit');
 			o.getOpts();
 			expect(outStr).toContain('Usage:');
 			expect(process.exit).toHaveBeenCalledWith(0);
@@ -77,7 +77,6 @@ describe('lib/index.js,', function() {
 		beforeEach(function() {
 			oldArgv = process.argv;
 			process.argv = [ 'node', __filename, '--help' ];
-			spyOn(process, 'exit');
 		});
 		afterEach(function() {
 			expect(process.exit).toHaveBeenCalledWith(0);
@@ -109,47 +108,63 @@ describe('lib/index.js,', function() {
 			expect(o.threshold.critical).toBe(60);
 			expect(o.threshold.warning).toBe(16);
 		});
-	});
 
-	it('calls method checkThreshold()', function() {
-		o.setThresholds({
-			'critical' : 60,
-			'warning' : 15
+		it('calls method checkThreshold()', function() {
+			o.setThresholds({
+				'critical' : 60,
+				'warning' : 15
+			});
+			var state = o.checkThreshold(61);
+			expect(state).toBe(o.states.CRITICAL);
 		});
-		var state = o.checkThreshold(61);
-		expect(state).toBe(o.states.CRITICAL);
-	});
-	it('calls methods addMessage() then checkMessages()', function() {
-		o.addMessage(o.states.CRITICAL, 'sky falling');
-		var messageObj = o.checkMessages();
-		expect(messageObj.message).toContain('sky falling');
-	});
-	describe('calls methods setThresholds() then addPerfdata()', function() {
-		o.setThresholds({
-			'critical' : 60,
-			'warning' : 15
+		it('calls methods addMessage() then checkMessages()', function() {
+			o.addMessage(o.states.CRITICAL, 'sky falling');
+			var messageObj = o.checkMessages();
+			expect(messageObj.message).toContain('sky falling');
 		});
-		o.addPerfData({
-			label : 'time',
-			value : 15,
-			uom : 's',
-			threshold : o.threshold
-		});
-		it('', function() {
-			expect(o.perfData[o.perfData.indexOfObject('label', 'time')].uom).toBe(
-					's');
-		});
-		describe('then getRetureMessage()', function() {
-			var msg = o.getRetureMessage(2, 'failure');
+
+		describe('calls methods setThresholds() then addPerfdata()', function() {
+			beforeEach(function() {
+				o.setThresholds({
+					'critical' : 60,
+					'warning' : 15
+				});
+				o.addPerfData({
+					label : 'time',
+					value : 15,
+					uom : 's',
+					threshold : o.threshold
+				});
+			});
 			it('', function() {
-				expect(msg).toContain('CRITICAL - failure|time=15s;15;60');
+				expect(o.perfData[o.perfData.indexOfObject('label', 'time')].uom).toBe(
+						's');
 			});
-			it('then nagiosExit()', function() {
-				spyOn(process, 'exit');
-				o.nagiosExit(2, 'failure');
-				expect(process.exit).toHaveBeenCalledWith(2);
-				expect(outStr.trim()).toBe(msg);
+			describe('then getRetureMessage()', function() {
+				var msg;
+				beforeEach(function() {
+					msg = o.getRetureMessage(2, 'failure');
+				});
+				it('', function() {
+					expect(msg).toContain('CRITICAL - failure|time=15s;15;60');
+				});
+				it('then nagiosExit()', function() {
+					o.nagiosExit(2, 'failure');
+					expect(process.exit).toHaveBeenCalledWith(2);
+					expect(outStr.trim()).toBe(msg);
+				});
 			});
 		});
+		it('calls method addArg() with a required argument then getOpts()',
+				function() {
+					o.addArg({
+						'spec' : 'm|myArg',
+						'help' : 'my argument',
+						'required' : true
+					});
+					o.getOpts();
+					expect(process.exit).toHaveBeenCalledWith(3);
+					expect(outStr).toContain('missing --myArg');
+				});
 	});
 });
--- test.js
+++ test.js
@@ -1,4 +1,12 @@
 'use strict';
 var F = require('./lib/index.js');
-var o = new F({a:"pass", usage:"sdf"});
+var o = new F({
+	a : "pass",
+	usage : "sdf"
+});
+o.addArg({
+	'spec' : 'm|myArg',
+	'help' : 'my argument',
+	'required' : true
+});
 o.getOpts();
