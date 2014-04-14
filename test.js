f69dcd023f353e119707c766388294aeaeafbe9b
abbr <wenfred@gmail.com>
2014-04-14 15:55:56 -0700

    replaced node-getopt with commander

--- test.js
+++ test.js
@@ -1,25 +1,29 @@
 'use strict';
-var Plugin = require('./lib/index.js');
+// add a command line options parser; you can substitute with your own favorite
+var program = require('commander');
+program
+	.version('0.0.1')
+	.usage('[Options] -- <arguments passed to wget>')
+	.option('-m, --match <string>', 'String response body must match')
+	.option('-w, --warning <float>', 'Warning threshold')
+	.option('-c, --critical <float>', 'Critical threshold')
+	.parse(process.argv);
 // create a new plugin object with optional initialization parameters
+var Plugin = require('./lib/index.js');
 var o = new Plugin({
 	// shortName is used in output
-	shortName : 'wget_http',
-	args : [ [ '', 'wget=<STRING>', 'Arguments passed to wget' ],
-			[ 'm', 'match=<STRING>', 'String response body must match' ],
-			[ 'w', 'warning=<STRING>', 'Warning threshold' ],
-			[ 'c', 'critical=<STRING>', 'Critical threshold' ],
-			[ 'h', 'help', 'display this help' ] ]
+	shortName : 'wget_http'
 });
 // set monitor thresholds
 o.setThresholds({
-	'critical' : o.parsedArgs.options.critical || 2,
-	'warning' : o.parsedArgs.options.warning || 0.2
+	'critical' : program.critical || 2,
+	'warning' : program.warning || 0.2
 });
 
 // run the check - replace with your own business logic
 var exec = require('child_process').exec;
 var before = new Date().getTime();
-exec('wget -qO- ' + o.parsedArgs.options.wget, function(error, stdout, stderr) {
+exec('wget -qO- ' + program.args.join(' '), function(error, stdout, stderr) {
 	var after = new Date().getTime();
 	var diff = (after - before) / 1000;
 
@@ -28,12 +32,10 @@ exec('wget -qO- ' + o.parsedArgs.options.wget, function(error, stdout, stderr) {
 	var state = o.checkThreshold(diff);
 	// Add message for later output. Multiple messages
 	// in the same state are concatenated at output
-	o.addMessage(state, stdout.length + ' bytes in ' + diff
-			+ ' seconds response time.');
+	o.addMessage(state, stdout.length + ' bytes in ' + diff + ' seconds response time.');
 	// use get() method to retrieved parsed program arguments
-	if (o.parsedArgs.options.match
-			&& stdout.indexOf(o.parsedArgs.options.match) === -1) {
-		o.addMessage(o.states.CRITICAL, o.parsedArgs.options.match + ' not found');
+	if (program.match && stdout.indexOf(program.match) === -1) {
+		o.addMessage(o.states.CRITICAL, program.match + ' not found');
 	}
 	// Add performance data
 	o.addPerfData({
